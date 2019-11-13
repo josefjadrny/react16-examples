aad94bb4643d4504815ad5311b0fba90e4ed9e22
Josef Jadrny <bc.josefjadrny@gmail.com>
2019-11-13 22:26:35 +0100

    typo

--- src/newReturnTypes.js
+++ src/newReturnTypes.js
@@ -3,7 +3,7 @@ import React from 'react'
 const NewReturnTypes = () => {
   return [
     <h1>New return types</h1>,
-    <p>You can new use arrays as a return type in render method</p>,
+    <p>You can use arrays as a return type in render method</p>,
   ]
 }
 
